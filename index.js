e5ec3934cd6ec5224cdd44f3535f7c6ed95ddf6c
Antti Haikonen <kpuisto@gmail.com>
2025-04-28 14:41:55 +0300

    moving json files to data folder

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 // hae data 
 app.get('/api/content/:page', async (req, res) => {
   try{
-    const data = await fs.readFile('./' + req.params.page + '.json', 'utf-8')
+    const data = await fs.readFile('./data/' + req.params.page + '.json', 'utf-8')
     res.json(data).end()
   } catch (e){
     console.log('error in getting page data', e)
@@ -15,21 +15,6 @@ app.get('/api/content/:page', async (req, res) => {
   }
     })
 
- // Pinkoodin lukeminen txt tiedostosta pelvelimelta ja lähettäminen selaimelle   
-//  app.get('/api/getpin', async (req, res) => {
-//     try {
-//         // Read the content of the text file
-//         const savedPin = await fs.readFile('./pin.txt', 'utf-8')
-        
-//         // Send the file content as the response
-//         res.json(savedPin)
-//     } catch (error) {
-//         console.error('Error reading file:', error)
-//         res.status(500).send('Internal Server Error')
-//     }
-//     })
-
-//-------------------------------------------------------
 // Tehdään polkumääritys public kansioon 
 const polku = path.join(__dirname, './public')
 
@@ -37,5 +22,5 @@ const polku = path.join(__dirname, './public')
 app.use(express.static(polku))
 
 app.listen(3004, () => {
-    console.log('Server is up on port 3004.')
+    console.log('Server (Careeria js basic) is up on port 3004.')
 })
